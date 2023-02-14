b845641367594b0721e88b78216614bf2b65bc2a
Daniel Leone <72521528+Daniel-Leone@users.noreply.github.com>
2023-02-14 14:05:59 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,15 @@
-public class Main {
-
-    // void function not have return
-    // public static void main(String[] args) {
-        
-    //     int resultado;
-    //     resultado = suma(10, 30);
-    //     System.out.println(resultado);
-
-    //     String concatenated;
-    //     concatenated = sumaString("Hello ", "World!");
-    //     System.out.println(concatenated);
-    // }
-
-    // // funcion estática que devuelve un entero(int) llamada suma.
-    // public static int suma(int a, int b){
-    //     return a + b;
-    // }
-
-    // public static String sumaString(String x, String y){
-    //     var result = x + y;
-    //     return result;
-    // }
-    
+public class Main {    
     public static void main(String[] args){
         sumarNumeros(18, 12, 2022);
         
         Coche miCoche = new Coche();
-        miCoche.puertas += 1;
-        System.out.println(miCoche.puertas);
+        miCoche.agregarPuertas();
+        System.out.println("el numero de puertas de mi coche es de: " + miCoche.puertas);
     }
     
     public static int sumarNumeros(int a, int b, int c){
         return a + b + c;
-    }
-    
-    
+    }   
 }
 
 class Coche{
